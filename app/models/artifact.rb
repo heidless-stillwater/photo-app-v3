fa88a6f260d764e34ec9fc08d2f27c8b5c53af94
heidless <lockhart.r@gmail.com>
2025-04-02 11:05:39 +0100

    Artifact.rb : artimg validations in place

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,6 +1,25 @@
 class Artifact < ApplicationRecord
   belongs_to :user#
-  has_one_attached :artimg  #for single image upload
+
+  has_one_attached :artimg do |attachable|
+    attachable.variant :thumb, resize_to_limit: [238, 238]
+  end
 
   # mount_uploader :artifact, ArtifactUploader
+
+  validates :name, presence: true
+  validates :artimg, presence: true
+
+  validate :artimg_size_limit
+
+  private
+
+  def artimg_size_limit
+    return unless artimg.attached?
+    if artimg.blob.byte_size > 5.megabytes
+      errors.add(:artimg, "must be less than 1MB")
+    end
+  end
+
+
 end
